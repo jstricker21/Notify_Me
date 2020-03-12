4d8ee5540e2bf604524858c07b05b5792bdb1761
Jacob Stricker <jstricker98@yahoo.com>
2020-03-12 10:49:11 -0400

    Created Button along with OnClickListener()

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.notifyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotifcation();
+            }
+        });
+    }
+
+    public void sendNotifcation() {
+
     }
 }
