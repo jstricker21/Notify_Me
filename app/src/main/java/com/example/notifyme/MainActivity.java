e5b41668ecbbc1ed63c1520c014841e8a6a8d052
Jacob Stricker <jstricker98@yahoo.com>
2020-03-12 11:12:03 -0400

    Created and set values for the notification

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
 
     private NotificationManager mNotifyManager;
+
+    private static  final int NOTIFICATION_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendNotification() {
-
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 
     public void createNotificationChannel() {
@@ -42,7 +47,19 @@ public class MainActivity extends AppCompatActivity {
             // Create a NotificationChannel
             NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
                     "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot!");
+            mNotifyManager.createNotificationChannel(notificationChannel);
         }
     }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_android);
+        return notifyBuilder;    }
 
 }
